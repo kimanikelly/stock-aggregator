4d857f19e3940da5d4604f0f7f70e39285c54776
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-10-27 19:24:15 -0400

    Fixed the import error preventing finnhub from accessing the client

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import click
-from api.finnhub import Stock_Data
+from finnhub_class import Stock_Data
 
 stock_data = Stock_Data()
+
 
 @click.group()
 def cli():
     pass
-    
+
+
 @cli.command("quote", help="Returns the current price of a stock ticker")
 @click.option("--ticker", prompt='Stock Ticker')
 def quote_endpont(ticker):
 
     if stock_data.quote(f"{ticker}") == 0:
-        click.echo(click.style(ticker,fg='red')+ ": Is not a valid stock ticker")
+        click.echo(click.style(ticker, fg='red') +
+                   ": Is not a valid stock ticker")
     else:
         result = stock_data.quote(f"{ticker}")
-        click.echo(click.style(f"{'$'}{result}",fg='green'))
-    
+        click.echo(click.style(f"{'$'}{result}", fg='green'))
 
- 
+
 if __name__ == "__main__":
     cli()
--- finnhub_class.py
+++ finnhub_class.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Stock_Data:
     def __init__(self):
-    
-        self.finnhub_client = finnhub.Client(api_key=os.getenv("FINNHUB_API_KEY"))
 
-    def quote(self,symbol):
+        self.finnhub_client = finnhub.Client(
+            api_key=os.getenv("FINNHUB_API_KEY"))
+
+    def quote(self, symbol):
         return self.finnhub_client.quote(symbol)['c']
