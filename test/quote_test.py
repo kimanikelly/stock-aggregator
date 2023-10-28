01289fe81b8cbacb113518e8fce60499f1bc89a3
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-10-27 20:09:13 -0400

    Quote command tests Test that verifies the class instance Test that verifies the stock price will be returned with a valid ticker Test that verifies 0 will be returned with an invalid ticker

---
+++ test/quote_test.py
@@ -0,0 +1,24 @@
+from finnhub_class import Stock_Data
+
+stock_data = Stock_Data()
+valid_ticker = "AMZN"
+invalid_ticker = "XYZ"
+
+
+def test_class_instance():
+
+    instance_status = isinstance(stock_data, Stock_Data)
+
+    assert (instance_status == True)
+
+
+def test_quote_for_valid_ticker():
+
+    assert (stock_data.quote(valid_ticker) > 0)
+
+    assert (type(stock_data.quote(valid_ticker)) == float)
+
+
+def test_quote_for_invalid_ticker():
+
+    assert (stock_data.quote(invalid_ticker) == 0)
